db12a84a05d281b9db76fe89a511c20e8a1f6185
ELCBAh <sebastianmaidana21@hotmail.com>
2025-09-21 13:07:46 -0300

    main character is now an image

--- main.py
+++ main.py
@@ -15,6 +15,9 @@ root = tk.Tk()
 root.title("Cookie Clicker")
 root.geometry("600x400")
 
+# Objects
+main_character_img = tk.PhotoImage(file="imgs/main_character.png")
+
 # Widgets
 
 # Label to show score
@@ -22,7 +25,7 @@ score_label = tk.Label(root, text="Score: 0", font=("Arial", 20))
 score_label.pack(pady=10)
 
 # Button to click on to increase score
-main_character = tk.Button(root, text="Main Character", width=10, height=5, command=main_character_click)
+main_character = tk.Button(root, image=main_character_img, width=200, height=252, command=main_character_click)
 main_character.pack(pady=20)
 
 root.mainloop()
